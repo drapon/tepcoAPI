9bc0f5ae23232f47d708fc6c6c5e66fccceae73b
drapon <ryuichi.hayashi@gmail.com>
2014-07-26 19:48:54 +0900

    add label text

--- createTodo/ViewController.swift
+++ createTodo/ViewController.swift
@@ -8,33 +8,76 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIApplicationDelegate {
                             
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
         let date = NSDate()
         
-        //現在の日付を取得
         let dateFormatter = NSDateFormatter()
         dateFormatter.locale = NSLocale(localeIdentifier: "ja")
         
         //書式を整える
         dateFormatter.dateFormat = "yyyy/MM/dd/HH"
         
-        //データをフォーマット
+       //データをフォーマット
         let d = dateFormatter.stringFromDate(date)
         
-        let origin:NSString = "http://tepco-usage-api.appspot.com/"
+       let origin:NSString = "http://tepco-usage-api.appspot.com/"
         let origin_url = origin + d + ".json"
-        
+      
         let url = NSURL(string: origin_url)
         let request = NSURLRequest(URL: url)
+        let connection: NSURLConnection = NSURLConnection(request: request, delegate: self, startImmediately: false)
         
         var data = NSURLConnection.sendSynchronousRequest(request, returningResponse: nil, error: nil)
-        var array = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableLeaves, error: nil) as NSDictionary
+        let dictionary = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableLeaves, error: nil) as NSDictionary
         
-        println(array)
+        
+        
+        func JSONStringify(jsonObj: AnyObject) -> String {
+            var e: NSError?
+            let jsonData = NSJSONSerialization.dataWithJSONObject(
+                jsonObj,
+                options: NSJSONWritingOptions(0),
+                error: &e)
+            if e {
+                return ""
+            } else {
+                return NSString(data: jsonData, encoding: NSUTF8StringEncoding)
+            }
+        }
+
+        
+        //Capacity
+        var capaLabel = UILabel(frame : CGRectMake(20, 200, 300, 20))
+        for (tepcoCode, tepcoName) in dictionary {
+            if(tepcoCode as NSString == "capacity"){
+               capaLabel.text = "最大値: \(tepcoName)"
+               var capaTepco = "\(tepcoName)"
+            }
+        }
+        self.view.addSubview(capaLabel)
+        
+        
+        //forecast_peak_usage
+        var peakLabel = UILabel(frame : CGRectMake(20, 200, 300, 100))
+        for (tepcoCode, tepcoName) in dictionary {
+            if(tepcoCode as NSString == "forecast_peak_usage"){
+                peakLabel.text = "利用電力: \(tepcoName)"
+                let usedTepco = "\(tepcoName)"
+            }
+        }
+        self.view.addSubview(peakLabel)
+        
+        //残り
+//        var zanLabel = UILabel(frame : CGRectMake(20, 200, 300, 180))
+//        zanLabel.text = "aaaa"
+//        self.view.addSubview(zanLabel)
+
+        
         
     }
 
